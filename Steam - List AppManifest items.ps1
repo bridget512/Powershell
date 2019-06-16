495c0fd9a9fb73c4f246ce0d0703f6ca338561a2
Bridget <51479040+bridget512@users.noreply.github.com>
2019-06-16 18:34:11 +1000

    Add files via upload

---
+++ Steam - List AppManifest items.ps1
@@ -0,0 +1,31 @@
+﻿Clear-Host
+
+Write-Host "Navigate to the steamapps folder"
+Write-Host "E.g. D:\Steam\SteamLibrary\steamapps"
+
+function Dialog-OpenFolder(){
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $Select_Folder = New-Object System.Windows.Forms.FolderBrowserDialog
+    $Select_Folder.ShowDialog()
+    $Select_Folder.RootFolder
+    cd  $Select_Folder.SelectedPath
+}
+
+Dialog-OpenFolder
+    
+
+$num = 0
+$names = Select-String -path *.acf -Pattern "name"
+
+Write-Host "ID`t FileName                                                           Game "
+
+ForEach($name in $names){
+
+    Write-Host "$num`t $($name)"
+    $num++
+
+}
+
+pause
+
